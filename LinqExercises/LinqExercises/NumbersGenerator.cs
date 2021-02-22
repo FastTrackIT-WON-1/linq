99007b9903f086c234586bbcf73c31396c37ea56
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-02-22 19:25:31 +0200

    Initial project setup and a first LINQ query

---
+++ LinqExercises/LinqExercises/NumbersGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace LinqExercises
+{
+    public static class NumbersGenerator
+    {
+        public static IEnumerable<int> Next()
+        {
+            int start = 0;
+            while (true)
+            {
+                yield return start;
+                start++;
+            }
+        }
+    }
+}
---
+++ LinqExercises/LinqExercises/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("N=");
+            string nValueAsString = Console.ReadLine();
+            if(!int.TryParse(nValueAsString, out int n))
+            {
+                throw new ArgumentException($"'{n}' must be a numeric value");
+            }
+
+            // IEnumerable<int> query = NumbersGenerator.Next().Where(nr => nr % 2 == 0);
+            IEnumerable<int> query = from nr in NumbersGenerator.Next()
+                        where nr % 2 == 0
+                        select nr;
+
+            int count = 0;
+            foreach (int element in query)
+            {
+                Console.WriteLine(element);
+                count++;
+
+                if(count >= n)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
